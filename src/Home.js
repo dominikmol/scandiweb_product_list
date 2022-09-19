24ab8682acbd8c3f7990837a2084df342cf8430c
Dominik Molenda <molenda.dominik@yahoo.com>
2022-09-19 15:35:16 +0200

    update

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios'
 
 function Home() {
   //  declaration of variables
-  let navigate = useNavigate();
   const [items, setItems] = useState([]);
   const [data, setData] = useState({
     action:"delete"
